88669fa02778bf046a0baf1e0c1f62aecb85109b
Jogis <ronipihajoki@proton.me>
2025-10-20 16:49:31 +0300

    idk even

--- Vertaisarviointi/src/login.java
+++ Vertaisarviointi/src/login.java
@@ -1,36 +1,36 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
-public class App {
+public class login {
     public static void main(String[] args) throws Exception {
+        
         Scanner in = new Scanner (System.in);
-
         String sukunimi = "";
         String etunimi = "";
         String tarkistus = "";
         String verkkotunnus = "";
         
         System.out.println("etunimi? ");
-        etunimi = in.nextLine();
+            etunimi = in.nextLine();
 
         System.out.println("sukunimi? ");
-        sukunimi = in.nextLine();
+            sukunimi = in.nextLine();
 
         System.out.println("Yrityksen verkkotunnus? ");
-        verkkotunnus = (in.nextLine());
+            verkkotunnus = (in.nextLine());
 
         System.out.println("Annoit seuraavat tiedot: " + etunimi + " " + sukunimi + verkkotunnus + " " + " Jos tiedot ovat Oikein kirjoita O, Väärin kirjoita V.");
-        tarkistus = in.nextLine();
+            tarkistus = in.nextLine();
+
     if (!tarkistus.equalsIgnoreCase("O")){
         System.out.println("Error: <Please Try Again>");
+        }
+    GenerateEmail(etunimi, sukunimi, verkkotunnus);
     }
-
+public static void GenerateEmail(String etunimi, String sukunimi, String verkkotunnus)
+{
+    System.out.println("Email is " + etunimi + "." + sukunimi + "@" + verkkotunnus);
 }
 }
-// public static void GenerateEmail(String email){
-//      System.out.println("Email is " + etunimi + "." + sukunimi + "@" + email)
-// }
-
 
 // GenerateEmail-metodi
 //     Metodin kutsuminen: 1 piste 
