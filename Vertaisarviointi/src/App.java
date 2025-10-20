d86b5a5ad31a9dbe8fabdfd918909720344223f2
Jogis <ronipihajoki@proton.me>
2025-10-20 14:10:57 +0300

    0.2

--- Vertaisarviointi/src/App.java
+++ Vertaisarviointi/src/App.java
@@ -8,19 +8,19 @@ public class App {
         String sukunimi = "";
         String etunimi = "";
         String tarkistus = "";
+        String verkkotunnus = "";
         do{
-        System.out.println("Hei antaisitko minulle etunimesi?");
+        System.out.println("etunimi? ");
         etunimi = in.nextLine();
 
-        System.out.println("Antaisitko minulle vielä sukunimesi?");
+        System.out.println("sukunimi? ");
         sukunimi = in.nextLine();
 
-        System.out.println("ja lopuksi sahkopostin pääte osoitteen?");
-        String email = (in.nextLine());
+        System.out.println("Yrityksen verkkotunnus? ");
+        verkkotunnus = (in.nextLine());
 
-        System.out.println("Annoit seuraavat tiedot: " + etunimi + " " + sukunimi + email + " Jos tiedot ovat Oikein kirjoita O, Väärin kirjoita V.");
+        System.out.println("Annoit seuraavat tiedot: " + etunimi + " " + sukunimi + verkkotunnus + " " + " Jos tiedot ovat Oikein kirjoita O, Väärin kirjoita V.");
         tarkistus = in.nextLine();
-         System.out.println("Email is " + etunimi + "." + sukunimi + "@" + email);
     } while (!tarkistus.equalsIgnoreCase("O"));
 
 }
@@ -31,26 +31,17 @@ public class App {
 
 
 // GenerateEmail-metodi
-
 //     Metodin kutsuminen: 1 piste 
-
 //     Metodin parametrit: 1 piste 
-
 //     Sähköpostiosoitteen muodostaminen: 1 piste 
-
 //     Merkkien muuttaminen pieniksi kirjaimiksi: 1 piste 
 
 // GenerateUsername-metodi
-
 //     Etunimestä 4 ensimmäistä merkkiä: 2 pistettä 
-
 //     Sukunimestä 4 viimeistä merkkiä: 2 pistettä 
 
 // Ohjelmointityyli 
-
 //     Koodissa on 2 tai useampia kommentteja ja kommentit ovat merkityksellisiä: 1 piste 
-
 //     Koodi on “kauniisti” ja systemaattisesti sisennetty: 1 piste 
-
 //     Muuttujien ja metodien nimet ovat kuvaavia: 1 piste 
 
