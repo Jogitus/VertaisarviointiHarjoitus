3ad1541a43853b28cd1ab9fbac0990e933198b6e
Jogis <ronipihajoki@proton.me>
2025-10-20 17:21:06 +0300

    1.0

--- Vertaisarviointi/src/login.java
+++ Vertaisarviointi/src/login.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 public class login {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception { // Main alkaa
         
         Scanner in = new Scanner (System.in);
         String sukunimi = "";
         String etunimi = "";
         String tarkistus = "";
         String verkkotunnus = "";
-        
+// Seuraavat kolme kysyvät käyttäjältä tietoja.
         System.out.println("etunimi? ");
             etunimi = in.nextLine();
 
@@ -16,8 +16,9 @@ public class login {
             sukunimi = in.nextLine();
 
         System.out.println("Yrityksen verkkotunnus? ");
-            verkkotunnus = (in.nextLine());
+            verkkotunnus = in.nextLine();
 
+// Seuraavat kolme tarkistavat onhan kaikkiin kohtiin vastattu. Eli ei tule "  " tyhjiä palautuksia.
     if (etunimi.isEmpty()) {
         System.out.println( "Virhe. Aloita operaatio alusta.");
         in.close();
@@ -34,27 +35,26 @@ public class login {
         return;
     }
 
-
+// Nämä kaksi methdoin kutsumista tulostavat annetuilla tiedoilla sähköpostin ja käyttäjänimen.
     GenerateEmail(etunimi, sukunimi, verkkotunnus);
     GenerateUsername(etunimi, sukunimi);
-    }
+    } // Main Loppuu
+
 public static void GenerateEmail(String etunimi, String sukunimi, String verkkotunnus)
 {
 
-    String sposti = (etunimi + "." + sukunimi + "@" + verkkotunnus).toLowerCase();
-    System.out.println(sposti);
-}
+    String sposti = (etunimi + "." + sukunimi + "@" + verkkotunnus).toLowerCase(); // Muotoilee sähköposti osoitteen ja laittaa kaiken lower case kirjoitus tyyliin.
+    System.out.println(sposti); // Tulostaa sähköposti osoitteen kokonaisuudessaan.
+} //Sähköpostin generointi methodi loppuu.
+
+
 public static void GenerateUsername(String etunimi, String sukunimi)
 {
-    String etun = etunimi.substring(0, 4);
-    String suku = sukunimi.substring(sukunimi.length() - 4);
-    String tunnus = (etun + suku).toLowerCase();
-    System.out.println(tunnus);
-}
+    String etunimen4 = etunimi.substring(0, 4); // Ottaa etunimestä merkit väliltä 0-4. eli nimen neljä ensimmäistä merkkiä.
+    String sukunimen4 = sukunimi.substring(sukunimi.length() - 4); // Ottaa sukunimen pituuden (esimerkiksi pihajoki on 8 merkkiä) ja vähentää siitä halutun 4. Tämä vähennettävä luku on siis se joka tästä tulostuu.
+    String tunnus = (etunimen4 + sukunimen4).toLowerCase(); //  Yhdistetään vähennetyt nimet ja muutetaan kaikki lowercase.
+    System.out.println(tunnus); // Tulostaa käyttäjätunnuksen kokonaisuudessaan.
+}// Käyttäjänimen generointi methodi loppuu.
 }
 
-// Ohjelmointityyli 
-//     Koodissa on 2 tai useampia kommentteja ja kommentit ovat merkityksellisiä: 1 piste 
-//     Koodi on “kauniisti” ja systemaattisesti sisennetty: 1 piste 
-//     Muuttujien ja metodien nimet ovat kuvaavia: 1 piste 
 
